bd668f5e482f7bdaa055031e497cb39582262234
sksazid01 <107117503+sksazid01@users.noreply.github.com>
2024-03-05 01:13:06 +0600

    Update Poke_Back_Code.py

--- Poke_Back_Code.py
+++ Poke_Back_Code.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.get("replace this") #Facebook Link here
+driver.get("https://mbasic.facebook.com/pokes/") #Facebook Link here
 time.sleep(1)
 
 
@@ -18,8 +18,8 @@ login.submit()
 time.sleep(2)
 
 for it in range(1,2000):
-   #poke Link provide here.
-   driver.get("Poke link here") #link for each person
-   driver.get("Poke link here") #link for each person
+   # Included screenshot in this repository
+   driver.get("") #poke link for each person 
+   driver.get("") #poke link for each person
     
 driver.quit()
